4b76747200f7b1ab302b8ef711e2e983412334f2
weebhacker <weebhacker1@gmail.com>
2022-12-13 13:08:31 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+//  var a = 10.5;
+//  console.log(Math.floor(a));
+//  console.log(Math.ceil(a));
+//  console.log(Math.round(a));
+var files = document.querySelector(".text");
+var random = Math.floor(Math.random()*100);
+console.log(random);
+files.textContent = random;
+
